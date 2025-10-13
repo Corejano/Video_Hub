9d2556ee60a06db1c941fad148aea9493ab17434
Corefano <corefan73@gmail.com>
2025-10-13 09:22:08 +0400

    apps

---
+++ apps/accounts/apps.py
@@ -0,0 +1,19 @@
+# apps/accounts/apps.py
+
+"""
+Accounts application configuration.
+"""
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    """Configuration for the accounts application."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.accounts'
+    verbose_name = 'User Accounts'
+
+    def ready(self):
+        """Import signals when the app is ready."""
+        import apps.accounts.signals
