6509f0f9a18460535f6d46180ab835ab3a1d4809
Corefano <corefan73@gmail.com>
2025-10-15 13:20:25 +0400

    movies

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', views.password_reset_done, name='password_reset_done'),
     path('password-reset-confirm/<str:token>/',
-            views.CustomPasswordResetConfirmView.as_view(),
-            name='password_reset_confirm'),
+         views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
     path('password-reset-complete/', views.password_reset_complete, name='password_reset_complete'),
 
     # Profile URLs
